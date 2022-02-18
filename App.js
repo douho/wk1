54aa25da387ce8ad69f95f5c503095c697252630
s110919032@stu.ntue.edu.tw <s110919032@stu.ntue.edu.tw>
2022-02-18 09:57:45 +0800

    name

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>110919032</Text>
+      <Text>數位二甲</Text>
+      <Text>胡竇琳 </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E69772',
     alignItems: 'center',
     justifyContent: 'center',
   },
