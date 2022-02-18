235aeef2ab0f34611e57cdcad7c84dc8586542f7
s110919032@stu.ntue.edu.tw <s110919032@stu.ntue.edu.tw>
2022-02-18 15:05:20 +0800

    name2

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>110919032</Text>
       <Text>數位二甲</Text>
-      <Text>胡竇琳</Text>
+      <Text>胡竇琳  </Text>
+      <Image></Image>
       <StatusBar style="auto" />
     </View>
   );
