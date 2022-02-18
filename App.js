7042f214a5f13f119a7073a6f177173703388898
s110919032@stu.ntue.edu.tw <s110919032@stu.ntue.edu.tw>
2022-02-18 09:58:27 +0800

    name

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>110919032</Text>
       <Text>數位二甲</Text>
-      <Text>胡竇琳 </Text>
+      <Text>胡竇琳</Text>
       <StatusBar style="auto" />
     </View>
   );
