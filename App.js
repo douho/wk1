a9c3d8e7c7d50fadcc097b90bf9513e60e1cc018
s110919032@stu.ntue.edu.tw <s110919032@stu.ntue.edu.tw>
2022-02-21 13:00:12 +0800

    pic

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+//import { Image } from "react-native-web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>110919032</Text>
-      <Text>數位二甲</Text>
-      <Text>胡竇琳</Text>
-      <View>
-
+      <Text style={styles.title}>110919032</Text>
+      <Text style={styles.title}>數位二甲</Text>
+      <Text style={styles.title}>胡竇琳</Text>
+      <View style={styles.a}>
+      <Image style={styles.img} source={require("./assets/pingue.jpg")} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -22,4 +23,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: '#fff',
+    fontSize: 40,
+  },
+  a:{
+    width: '100%',
+    display:"flex",
+    justifyContent:'center',
+    // alignContent:"center",
+  },
+  img:{
+    alignSelf:'center',
+    width: '50%',
+    height: '50%',
+    // position: 'absolute',
+  }
 });
